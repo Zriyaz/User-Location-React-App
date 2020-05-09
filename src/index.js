977e3468fb55832dd2962bd8434cc2197f46d258
Md Riyaz Kazmi <mdriyazansari.18mca@kongu.edu>
2020-05-09 05:53:51 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React,{Component} from "react"
+import ReactDOM from "react-dom"
+
+ class App extends Component{
+  constructor(props){
+    super(props)
+    this.state={lat:null, errorMessage:''}
+    window.navigator.geolocation.getCurrentPosition(
+      position=>{
+        this.setState({lat:position.coords.latitude})
+        },
+      err=>{
+        this.setState({errorMessage:err.message})
+      }
+    )
+  }
+  render(){
+      if(this.state.errorMessage && !this.state.lat){
+        return <div>Error:{this.state.errorMessage}</div>
+      }
+      if(!this.state.errorMessage && this.state.lat ){
+        return <div>Latitude:{this.state.lat}</div>
+      }
+      return <div>Loading...</div> 
+    
+  }
+}
+ReactDOM.render(<App/>,document.querySelector("#root"))
+
